30983bca1956f719327f71b4dc4305ec60a547ce
John Bowser <john.bow7720@gmail.com>
2023-11-10 17:34:14 -0700

    setup eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,39 @@
-import * as fs from "fs";
-import "dotenv/config";
-import { compile } from "html-to-text";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { RecursiveUrlLoader } from "langchain/document_loaders/web/recursive_url";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { OpenAI } from "langchain/llms/openai";
+import 'dotenv/config';
+import { compile } from 'html-to-text';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { RecursiveUrlLoader } from 'langchain/document_loaders/web/recursive_url';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { OpenAI } from 'langchain/llms/openai';
 
 const llm = new OpenAI({
-  modelName: "gpt-3.5-turbo",
+  modelName: 'gpt-3.5-turbo',
   temperature: 0,
 });
 
-const url = "https://ecode360.com/26884347";
-console.time("execution time");
-console.log("Creating extractor");
+const url = 'https://ecode360.com/26884347';
+console.time('execution time');
+console.log('Creating extractor');
 
 const compiledConvert = compile({
   wordwrap: 130,
-  baseElements: { selectors: ["#main"] },
+  baseElements: { selectors: ['#main'] },
   selectors: [
-    { selector: "a", options: { ignoreHref: true } },
-    { selector: "a.xref", format: "skip" },
-    { selector: "img", format: "skip" },
+    { selector: 'a', options: { ignoreHref: true } },
+    { selector: 'a.xref', format: 'skip' },
+    { selector: 'img', format: 'skip' },
   ],
 });
 
-console.log("Creating loader");
+console.log('Creating loader');
 
 const loader = new RecursiveUrlLoader(url, {
   extractor: compiledConvert,
   maxDepth: 1,
-  excludeDirs: ["https://ecode360.com/26884347/attachment/"],
+  excludeDirs: ['https://ecode360.com/26884347/attachment/'],
 });
 
-console.log("Loading documents....");
+console.log('Loading documents....');
 
 const docs = await loader.load();
 
@@ -46,22 +45,22 @@ const splitter = new RecursiveCharacterTextSplitter({
 const docOutput = await splitter.splitDocuments(docs);
 const embeddings = new OpenAIEmbeddings();
 
-console.log("Loading documents into vector store");
+console.log('Loading documents into vector store');
 const vectorStore = await MemoryVectorStore.fromDocuments(
   docOutput,
-  embeddings
+  embeddings,
 );
 
-console.log("Vector store loaded");
+console.log('Vector store loaded');
 
-console.log("beginning query process...");
+console.log('beginning query process...');
 const question =
-  "If I need to get a special permit, how would I do so? What is the process?";
+  'If I need to get a special permit, how would I do so? What is the process?';
 
 const docVectors = await vectorStore.similaritySearch(question);
 const context = docVectors
-  .map((doc) => doc.pageContent.replace(/\n+/, "\n").trim())
-  .join("\n ------ \n");
+  .map(doc => doc.pageContent.replace(/\n+/, '\n').trim())
+  .join('\n ------ \n');
 const prompt = `You are a helpful assistant that loves to help people. Using only the context provided answer the question. If you cannot answer the question using only the provided context respond with "I'm sorry, I cannot answer that"
 Context:
 ${context}
@@ -74,4 +73,4 @@ If possible, please provide citations to related section number of the answer wa
 `;
 const res = await llm.call(prompt);
 console.log(res);
-console.timeEnd("execution time");
+console.timeEnd('execution time');
