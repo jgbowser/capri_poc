d158e0bc3efca82e96b614635f6e0d78f8587256
John Bowser <john.bow7720@gmail.com>
2023-11-17 06:33:37 -0700

    basic backend now works

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import { queryDocuments } from "./queryChain.js";
+import { queryDocuments } from './queryChain.js';
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.post('/query', async (req, res) => {
   if (!req.body.query) {
-    return res.status(400).json({ error: 'Missing query property in the request body' });
+    return res
+      .status(400)
+      .json({ error: 'Missing query property in the request body' });
   }
 
   const answer = await queryDocuments(req.body.query);
-}
+
+  res.json(answer);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
